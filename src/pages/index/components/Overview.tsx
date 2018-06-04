e0fdcb03bead69899a7104372527b4251a7d1d85
leroldary <leroldary@protonmail.com>
2018-06-04 20:33:37 +0200

    Add no details required text to login

--- src/pages/index/components/Overview.tsx
+++ src/pages/index/components/Overview.tsx
@@ -8,8 +8,9 @@ const Overview = () => (
     <div className={Style.overview}>
         <Container>
             <Jumbotron className={Style.jumbotron}>
-                <h1 className="text-center">The state channel dice casino</h1>
+                <h1> The state channel dice casino</h1>
                 <Button color="primary" size="lg">Join Now</Button>
+                <span className={Style.info}>No details required! Login with Metamask or similar!</span>
             </Jumbotron>
         </Container>
     </div>
