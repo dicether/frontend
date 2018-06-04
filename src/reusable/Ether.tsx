258101aa6c24512d7acf4e019ab57359e30d922f
leroldary <leroldary@protonmail.com>
2018-06-04 18:00:12 +0200

    Icon for ether

--- src/reusable/Ether.tsx
+++ src/reusable/Ether.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ClassNames from "classnames";
 
 const Style = require('./Ether.scss');
+const icon = require('assets/images/ETH_icon.svg');
 
 export function formatEth(gwei: number, precision = 9) {
     return (gwei / 1e9).toFixed(precision);
@@ -15,7 +16,12 @@ const Ether = ({gwei, showCurrencySymbol = true, colored = false}: { gwei: numbe
         {[Style.ether_negativ]: colored && gwei < 0}
     );
 
-    return <span className={classNames}>{ether}{showCurrencySymbol && ' ETH'}</span>;
+    return (
+        <span className={classNames}>
+            <span className={Style.value}>{ether}</span>
+            {showCurrencySymbol && <img  className={Style.icon} src={icon}/> }
+        </span>
+    )
 };
 
 export default Ether;
