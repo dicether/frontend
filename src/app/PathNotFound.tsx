4de18c917c9f41dd2af3f01fc725b8ab744821c3
leroldary <leroldary@protonmail.com>
2018-06-05 19:17:43 +0200

    Add 404 to Path does not exist message

--- src/app/PathNotFound.tsx
+++ src/app/PathNotFound.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const PathNotFound = () => (
-  <h1 className="text-danger text-center">Oops, this path does not exist!</h1>
+  <h1 className="text-danger text-center">404 Oops, this path does not exist!</h1>
 );
 
 export default PathNotFound;
