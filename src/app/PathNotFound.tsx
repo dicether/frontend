c38662cf9b71fdebc5487fa8830600935f707fa0
leroldary <leroldary@protonmail.com>
2018-06-06 10:57:05 +0200

    Add container to 404 component

--- src/app/PathNotFound.tsx
+++ src/app/PathNotFound.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import {Container} from "../reusable";
 
 const PathNotFound = () => (
-  <h1 className="text-danger text-center">404 Oops, this path does not exist!</h1>
+    <Container>
+        <h1 className="text-danger text-center">404 Oops, this path does not exist!</h1>
+    </Container>
 );
 
 export default PathNotFound;
