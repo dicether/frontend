6a6441335f1aed57369b7ca6b8a88251899fc5f8
leroldary <leroldary@protonmail.com>
2018-06-08 10:03:36 +0200

    Improve games entry

--- src/pages/index/components/Games.tsx
+++ src/pages/index/components/Games.tsx
@@ -12,10 +12,12 @@ const Games = () => (
         <Container>
             <h2 className="text-center">Games</h2>
             <div className={Style.gamesList}>
-                <Link to="/games/dice" className={Style.gamesListEntry + ' ' + Style.gamesListEntry_enabled}>
-                        <Icon className={Style.gamesListEntry__img} size="8x" icon="dice"/>
-                        <h5 className={Style.gamesListEntry__text}>Classic Dice</h5>
-                </Link>
+                <div className={Style.gamesListEntry}>
+                    <Link to="/games/dice" className={Style.gamesListEntry__link}>
+                            <Icon className={Style.gamesListEntry__img} size="8x" icon="dice"/>
+                            <h5 className={Style.gamesListEntry__text}>Classic Dice</h5>
+                    </Link>
+                </div>
                 <div className={Style.gamesListEntry + ' ' + Style.gamesListEntry_disabled}>
                     <Icon className={Style.gamesListEntry__img} size="8x" icon="question"/>
                     <h5 className={Style.gamesListEntry__text}>More Coming Soon</h5>
