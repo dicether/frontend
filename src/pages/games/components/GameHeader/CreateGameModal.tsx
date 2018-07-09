3109668d9dcd6738ab1de44700f98596677478b9
leroldary <leroldary@protonmail.com>
2018-07-09 15:42:34 +0200

    Improve createGameModal

--- src/pages/games/components/GameHeader/CreateGameModal.tsx
+++ src/pages/games/components/GameHeader/CreateGameModal.tsx
@@ -85,47 +85,45 @@ export default class CreateGameModal extends React.Component<Props, State> {
 
         return (
             <Modal isOpen={isOpen} toggle={onClose}>
-                    <h3 className="text-center">Create Game Session</h3>
-                    {accountBalance === null ?
-                        <span className={"text-warning"}>Failed reading your account balance!</span>
-                        :
-                        <span>Your Balance: <Ether gwei={accountBalance}/> ETH</span>
-                    }
-                    {toLowBalance ?
-                        <div>
-                            <span className={'text-danger'}>Too low balance on your account!</span>
-                        </div>
-                        :
-                        <Form style={{marginTop: '1em'}} onSubmit={this.createGame}>
-                            <FormGroup>
-                                <Label for="value">
-                                    Amount to deposit (between <Ether gwei={minValue} precision={2}/>
-                                    and <Ether gwei={max} precision={2}/> ETH)
-                                </Label>
-                                <ValueInput
-                                    id="value"
-                                    min={minValue}
-                                    max={max}
-                                    step={minValue}
-                                    value={value}
-                                    onChange={this.onValueChange}
-                                />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="seed">Your seed used to generate the hash chain</Label>
-                                <Input disabled value={seed}/>
-                            </FormGroup>
-                            <FormGroup className='text-warning'>
-                                The data required for the game session is stored local on your browser. So <em>don't clear
-                                your browser history</em> as long as the game session is active.
-                                If your are done with playing you must end the game session. If you don't end the game session,
-                                we will end it after waiting {SESSION_TIMEOUT} hours and you will need to pay a fine!
-                            </FormGroup>
-                            <FormGroup>
-                                <Button type="submit" color="primary" disabled={toLowBalance}>Deposit</Button>
-                            </FormGroup>
-                        </Form>
-                    }
+                <h3 className="text-center">Create Game Session</h3>
+                {accountBalance === null ?
+                    <p className={"text-warning"}>Failed reading your account balance!</p>
+                    :
+                    <p>Your Balance: <Ether gwei={accountBalance}/> ETH</p>
+                }
+                {toLowBalance ?
+                    <p className={'text-danger'}>Too low balance on your account!</p>
+                    :
+                    <Form onSubmit={this.createGame}>
+                        <FormGroup>
+                            <Label for="value">
+                                Amount to deposit (between <Ether gwei={minValue} precision={2}/>
+                                and <Ether gwei={max} precision={2}/> ETH)
+                            </Label>
+                            <ValueInput
+                                id="value"
+                                min={minValue}
+                                max={max}
+                                step={minValue}
+                                value={value}
+                                onChange={this.onValueChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="seed">Your seed used to generate the hash chain</Label>
+                            <Input disabled value={seed}/>
+                        </FormGroup>
+                        <FormGroup className='text-warning'>
+                            The data required for the game session is stored local on your browser. So <em>don't clear
+                            your browser history</em> as long as the game session is active.
+                            If your are done with playing you must end the game session. If you don't end the game session,
+                            we will end it after waiting {SESSION_TIMEOUT} hours and you will need to pay a fine!
+                        </FormGroup>
+                        <FormGroup>
+                            <Button type="submit" color="primary" disabled={toLowBalance}>Deposit</Button>
+                        </FormGroup>
+                    </Form>
+                }
             </Modal>
         );
     }
