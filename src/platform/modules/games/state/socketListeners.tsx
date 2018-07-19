b7b5d05187dc54e11087f22b8c0c0fec1162e930
leroldary <leroldary@protonmail.com>
2018-07-19 17:57:30 +0200

    Rename/add game session events

--- src/platform/modules/games/state/socketListeners.tsx
+++ src/platform/modules/games/state/socketListeners.tsx
@@ -1,13 +1,20 @@
 import {Dispatch} from "../../../../util/util";
-import {serverActiveGame} from "./asyncActions";
+import {loadContractGameState, serverActiveGame, syncGameState} from "./asyncActions";
+import {catchError} from "../../utilities/asyncActions";
 
 type GameAcceptType = {gameId: number, serverHash: string}
 type GameRejectType = {gameId: number, serverHash: string}
 
 
 const listeners = {
-    acceptedGameSession: (dispatch: Dispatch) => ({gameId, serverHash}: GameAcceptType ) => {
+    gameSessionActive: (dispatch: Dispatch) => ({gameId, serverHash}: GameAcceptType ) => {
         dispatch(serverActiveGame(gameId, serverHash));
+    },
+    gameSessionConflictEnded: (dispatch: Dispatch) => ({gameId}: GameAcceptType ) => {
+        dispatch(loadContractGameState()).catch(error => catchError(error, dispatch));
+    },
+    gameSessionEnded: (dispatch: Dispatch) => ({gameId}: GameAcceptType ) => {
+        dispatch(loadContractGameState()).catch(error => catchError(error, dispatch));
     }
 };
 
