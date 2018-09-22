2e6ee2949365474222c64e00b967cc2cd03b212e
leroldary <leroldary@protonmail.com>
2018-09-22 15:11:42 +0200

    Add local link support

--- src/platform/components/chat/components/Message.tsx
+++ src/platform/components/chat/components/Message.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {emojify} from 'react-emojione';
+import {Link} from "react-router-dom";
 import ClassNames from 'classnames';
 import moment from 'moment';
 
@@ -7,7 +8,6 @@ import {Dropdown, Popover} from '../../../../reusable/index';
 import UserType from './UserType';
 import UserToolTip from './UserMenu';
 import Bet from '../../bet/Bet';
-
 import {Message as MessageType} from '../../../modules/chat/types';
 import {Friend} from '../../../modules/friends/types';
 
@@ -19,12 +19,18 @@ const emojioneImage = require('assets/images/emojione-3.1.2-64x64.png');
 function processMessage(message: string) {
     const betRegex = /Bet:(\d+)/;
     const linkRegex = /(https?:\/\/\S+)/;
+    const host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+    const localLinkRegex = new RegExp(`${host}/(\\S+)`);
 
     const res1 = reactStringReplace(message, betRegex, (match) => (
         <Bet betId={match}/>
     ));
 
-    return reactStringReplace(res1, linkRegex, (match) => (
+    const res2 = reactStringReplace(res1, localLinkRegex, (match) => (
+        <Link to={match}>{`${host}/${match}`}</Link>
+    ));
+
+    return reactStringReplace(res2, linkRegex, (match) => (
         <a href={match}>{match}</a>
     ));
 }
