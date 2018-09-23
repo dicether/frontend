ee69c93a4b2d5cb7b61cda7797aa46acacb50194
leroldary <leroldary@protonmail.com>
2018-09-23 14:48:56 +0200

    Add DataLoader component

---
+++ src/reusable/DataLoader.tsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import axios from "axios";
+
+import Icon from "./FontAwesomeIcon";
+
+const Style = require("./DataLoader.scss");
+
+
+const Loading = () => (
+    <div className={Style.info}>
+        <Icon icon="spinner" spin size="lg"/>
+    </div>
+);
+
+const Failure = () => (
+    <div className={Style.info}>
+        <span className="text-danger">Error loading Data</span>
+    </div>
+);
+
+const NotFound = () => (
+    <div className={Style.info}>
+        <span className="text-danger">404 Not Found</span>
+    </div>
+);
+
+
+
+
+type Props<T> = {
+    url: string,
+    success(data: T): React.ReactNode,
+    loading?(): React.ReactNode,
+    notFound?(): React.ReactNode,
+    failure?(): React.ReactNode,
+};
+
+
+type State<T> = {
+    data: T|undefined,
+    error: any
+}
+
+
+class DataLoader<T = any> extends React.Component<Props<T>, State<T>> {
+    static defaultProps = {
+        loading: () => <Loading/>,
+        notFound: () => <NotFound/>,
+        failure: () => <Failure/>
+    };
+
+    constructor(props: Props<T>) {
+        super(props);
+
+        this.state = {
+            data: undefined,
+            error: undefined
+        }
+    }
+
+    componentDidMount() {
+        const {url} = this.props;
+
+        axios.get<T>(url).then(response => {
+            const data = response.data;
+            this.setState({data});
+        }).catch(error => {
+            this.setState({error})
+        });
+    }
+
+    render() {
+        const {failure, notFound, loading, success} = this.props;
+        const {data, error} = this.state;
+
+        if (data) {
+            return success(data);
+        } else if (error) {
+            if (error.response && error.response.status === 404) {
+                const status = error.response.status;
+                return notFound!();
+
+            } else {
+                return failure!();
+            }
+        } else {
+            return loading!();
+        }
+    }
+}
+
+export default DataLoader;
