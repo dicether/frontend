1d015d632237c49fc8dfb58b5c7a5b32561e9a07
leroldary <leroldary@protonmail.com>
2018-09-29 23:15:25 +0200

    Add share button to UserInfo

--- src/platform/components/user/UserInfo.tsx
+++ src/platform/components/user/UserInfo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import {Stats as StatsType, User} from "../../modules/account/types";
 import GameStats from "../../../pages/account/components/stats/GameStats";
-import {Address, DataLoader} from '../../../reusable';
+import {Address, CopyToClipBoard, DataLoader} from '../../../reusable';
 
 const Style = require('./UserInfo.scss');
 
@@ -21,7 +21,9 @@ export default class UserInfo extends React.PureComponent<Props> {
 
         return (
             <div className={Style.userInfo}>
-                <h3 className="text-center">{user.username}</h3>
+                <h3 className="text-center">
+                    {user.username} <CopyToClipBoard message={"Copied! Paste in Chat!"} content={`User:${user.username}`} />
+                </h3>
                 <Address address={user.address} />
                 <DataLoader<StatsType>
                     url={`/userStats/${user.address}`}
