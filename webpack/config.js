d64589767f44df1aad08d7d9bfa04b100d7352dc
leroldary <leroldary@protonmail.com>
2018-10-25 11:31:35 +0200

    Update contract address

--- webpack/config.js
+++ webpack/config.js
@@ -2,7 +2,7 @@ const domain = 'dicether.com';
 
 module.exports = {
     domain: domain,
-    contractAddress: "0xEB6F4eC38A347110941E86e691c2ca03e271dF3b",
+    contractAddress: "0x9919D97e50397b7483E9EA61e027E4C4419c8171",
     serverAddress: "0xcef260a5fed7a896bbe07b933b3a5c17aec094d8",
     apiUrl: `https://api.${domain}/api`,
     websocketUrl: `https://websocket.${domain}`,
