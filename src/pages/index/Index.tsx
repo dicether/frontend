e29dc95de73c694f9891123c34768aae2d8ef968
leroldary <leroldary@protonmail.com>
2018-11-02 21:39:00 +0100

    Remove Join button and small features if logged in

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -1,18 +1,38 @@
 import * as React from "react";
 import DocumentTitle from "react-document-title";
 
+import {connect} from "react-redux";
+import {bindActionCreators, Dispatch} from "redux";
 import Stats from "../../platform/components/bet/Stats";
+import {showRegisterModal} from "../../platform/modules/modals/actions";
 import {Container, Section} from "../../reusable";
+import {State as RootState} from "../../rootReducer";
 import BigFeatures from "./components/BigFeatures";
 import Games from "./components/Games";
 import Overview from "./components/Overview";
 import SmallFeatures from "./components/SmallFeatures";
 
-const Index = () => (
+const mapDispatchToProps = (dispatch: Dispatch) =>
+    bindActionCreators(
+        {
+            showRegisterModal,
+        },
+        dispatch
+    );
+
+export const mapStateToProps = (state: RootState) => {
+    return {
+        loggedIn: state.account.jwt !== null,
+    };
+};
+
+export type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+
+const Index = ({loggedIn, showRegisterModal}: Props) => (
     <DocumentTitle title="Dicether">
         <div>
-            <Overview />
-            <SmallFeatures />
+            <Overview loggedIn={loggedIn} showRegisterModal={showRegisterModal} />
+            {!loggedIn && <SmallFeatures />}
             <Games />
             <Section>
                 <Container>
@@ -24,4 +44,7 @@ const Index = () => (
     </DocumentTitle>
 );
 
-export default Index;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Index);
--- src/pages/index/components/Overview.tsx
+++ src/pages/index/components/Overview.tsx
@@ -1,37 +1,28 @@
 import * as React from "react";
-import {connect} from "react-redux";
 import {Jumbotron} from "reactstrap";
-import {bindActionCreators, Dispatch} from "redux";
-import {showRegisterModal} from "../../../platform/modules/modals/actions";
 import {Button, Container} from "../../../reusable";
 
 const Style = require("./Overview.scss");
 
-const mapDispatchToProps = (dispatch: Dispatch) =>
-    bindActionCreators(
-        {
-            showRegisterModal,
-        },
-        dispatch
-    );
+export type Props = {
+    loggedIn: boolean;
+    showRegisterModal(): void;
+};
 
-export type Props = ReturnType<typeof mapDispatchToProps>;
-
-const Overview = ({showRegisterModal}: Props) => (
+const Overview = ({loggedIn, showRegisterModal}: Props) => (
     <div className={Style.overview}>
         <Container>
             <Jumbotron className={Style.jumbotron}>
                 <h1> The state channel dice casino</h1>
-                <Button color="primary" size="lg" onClick={showRegisterModal}>
-                    Join Now
-                </Button>
+                {!loggedIn && (
+                    <Button color="primary" size="lg" onClick={showRegisterModal}>
+                        Join Now
+                    </Button>
+                )}
                 <span className={Style.info}>No details required! Login with Metamask or similar!</span>
             </Jumbotron>
         </Container>
     </div>
 );
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(Overview);
+export default Overview;
