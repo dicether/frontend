191a5d906a527cb96dc458d825708d81243d6fbf
leroldary <leroldary@protonmail.com>
2019-03-02 16:15:16 +0100

    Update dev contract address

--- webpack/dev.js
+++ webpack/dev.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 const common = require('./common');
 
 
-const contractAddress = "0xd22f70c9eace8873a28a61bd0338f82fd71ad636";
+const contractAddress = "0xe1E98F272C7085BB24279838fd75ecdb62edFc65";
 const serverAddress = "0xa8d5f39f3ccd4795b0e38feacb4f2ee22486ca44";
 const apiUrl = 'http://localhost:5000/api';
 const websocketUrl = 'http://localhost:5001';
