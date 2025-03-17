f71e4a939280d74d482936056e552382cde1f7c0
leroldary <leroldary@protonmail.com>
2025-03-17 10:47:54 +0100

    Use react hooks for Notification implementation

--- src/app/Notification.tsx
+++ src/app/Notification.tsx
@@ -3,6 +3,7 @@ import {toast, ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // tslint:disable-line:no-submodule-imports
 
 import * as Style from "./Notification.scss";
+import {useEffect} from "react";
 
 const CloseButton = () => <button type="button" className="btn-close btn-close-white" aria-label="Close" />;
 
@@ -10,38 +11,32 @@ export type Props = {
     notification: any;
 };
 
-export default class Notification extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
+const Notification = (props: Props) => {
+    useEffect(() => {
+        const notification = props.notification;
+        if (notification == null) return;
 
-    componentWillReceiveProps(nextProps: Props) {
-        if (nextProps.notification !== this.props.notification && nextProps.notification !== null) {
-            const notification = nextProps.notification;
-            if (notification.type === "success") {
-                toast.success(<div style={{width: "275px", wordWrap: "break-word"}}>{notification.message}</div>);
-            } else if (notification.type === "info") {
-                toast.info(<div style={{width: "275px", wordWrap: "break-word"}}>{notification.message}</div>);
-            } else {
-                toast.error(
-                    /*<div style={{width: "275px", wordWrap: "break-word"}}>{*/ notification.message /*}</div>*/,
-                );
-            }
+        if (notification.type === "success") {
+            toast.success(<div style={{width: "275px", wordWrap: "break-word"}}>{notification.message}</div>);
+        } else if (notification.type === "info") {
+            toast.info(<div style={{width: "275px", wordWrap: "break-word"}}>{notification.message}</div>);
+        } else {
+            toast.error(/*<div style={{width: "275px", wordWrap: "break-word"}}>{*/ notification.message /*}</div>*/);
         }
-    }
+    }, [props.notification]);
 
-    render() {
-        return (
-            <ToastContainer
-                icon={false}
-                toastClassName={Style.notification}
-                position="top-left"
-                autoClose={5000}
-                hideProgressBar={true}
-                closeOnClick
-                pauseOnHover={false}
-                closeButton={<CloseButton />}
-            />
-        );
-    }
-}
+    return (
+        <ToastContainer
+            icon={false}
+            toastClassName={Style.notification}
+            position="top-left"
+            autoClose={5000}
+            hideProgressBar={true}
+            closeOnClick
+            pauseOnHover={false}
+            closeButton={<CloseButton />}
+        />
+    );
+};
+
+export default Notification;
