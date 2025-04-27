a57f5e267e2161d368d4e3728c3c1fe919b5ae7b
leroldary <leroldary@protonmail.com>
2025-04-27 10:13:00 +0200

    Do not show header banner after login

--- src/pages/index/components/Overview.tsx
+++ src/pages/index/components/Overview.tsx
@@ -13,15 +13,17 @@ export interface Props extends WithTranslation {
 const Overview = ({loggedIn, showRegisterModal, t}: Props) => (
     <div className={Style.overview}>
         <Container>
-            <div className={Style.jumbotron + " rounded px-3 px-sm-4 py-3 py-sm-5"}>
-                <h1>{t("theStateChannelCasino")}</h1>
-                {!loggedIn && (
+            {!loggedIn && (
+                <div className={Style.jumbotron + " rounded px-3 px-sm-4 py-3 py-sm-5"}>
+                    <h1>{t("theStateChannelCasino")}</h1>
+
                     <Button color="primary" size="lg" onClick={showRegisterModal}>
                         {t("JoinNow")}
                     </Button>
-                )}
-                <span className={Style.info}>{t("noDetailsRequired")}</span>
-            </div>
+
+                    <span className={Style.info}>{t("noDetailsRequired")}</span>
+                </div>
+            )}
         </Container>
     </div>
 );
