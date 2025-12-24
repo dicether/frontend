936e638ef18643cf4d76821f8b5baea23947b367
leroldary <leroldary@protonmail.com>
2025-12-24 10:27:08 +0100

    Prepare wagmi

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,13 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import * as React from "react";
 import {Provider} from "react-redux";
 import {BrowserRouter as Router} from "react-router-dom";
 import {Store} from "redux";
+import {WagmiProvider} from "wagmi";
 
 import App from "./app/App";
 import withTracker from "./app/withTracker";
+import wagmiConfig from "./config/wagmiConfig";
 import {State} from "./rootReducer";
 
 export interface Props {
@@ -13,14 +16,20 @@ export interface Props {
 
 const TrackedApp = withTracker(App);
 
+const queryClient = new QueryClient();
+
 export default class Root extends React.Component<Props> {
     render() {
         return (
-            <Provider store={this.props.store}>
-                <Router>
-                    <TrackedApp />
-                </Router>
-            </Provider>
+            <WagmiProvider config={wagmiConfig}>
+                <QueryClientProvider client={queryClient}>
+                    <Provider store={this.props.store}>
+                        <Router>
+                            <TrackedApp />
+                        </Router>
+                    </Provider>
+                </QueryClientProvider>
+            </WagmiProvider>
         );
     }
 }
