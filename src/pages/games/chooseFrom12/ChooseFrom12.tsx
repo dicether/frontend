ceb8b1875f220d36f083d3e370e51159e7241655
leroldary <leroldary@protonmail.com>
2025-12-27 11:17:08 +0100

    Convert ChooseFrom12 to functional component

--- src/pages/games/chooseFrom12/ChooseFrom12.tsx
+++ src/pages/games/chooseFrom12/ChooseFrom12.tsx
@@ -1,173 +1,146 @@
 import {GameType, maxBet} from "@dicether/state-channel";
 import * as React from "react";
+import {useEffect} from "react";
 import {Helmet} from "react-helmet";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 
 import {KELLY_FACTOR, MAX_BET_VALUE, MIN_BANKROLL, MIN_BET_VALUE} from "../../../config/config";
 import {addNewBet} from "../../../platform/modules/bets/asyncActions";
-import {Bet} from "../../../platform/modules/bets/types";
 import {toggleHelp} from "../../../platform/modules/games/info/actions";
 import {placeBet, validChainId} from "../../../platform/modules/games/state/asyncActions";
 import {showErrorMessage} from "../../../platform/modules/utilities/actions";
 import {catchError} from "../../../platform/modules/utilities/asyncActions";
 import {State} from "../../../rootReducer";
-import {Dispatch} from "../../../util/util";
+import {useDispatch} from "../../../util/util";
 import sounds from "../sound";
 import {canPlaceBet} from "../utilities";
 import {changeNum, changeValue} from "./actions";
 import Ui from "./components/Ui";
 import {playFromBegin} from "../../../util/audio";
 
-const mapStateToProps = ({games, account, web3}: State) => {
-    const {info, oneDice, gameState} = games;
-    const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
+const ChooseFrom12 = () => {
+    const loadedSounds = React.useRef(false);
+    const resultTimeoutId = React.useRef(0);
+    const [showResult, setShowResult] = React.useState(false);
+    const [result, setResult] = React.useState({num: 0, won: false});
 
-    return {
-        web3Available: web3Available === true,
-        info,
-        oneDice,
-        gameState,
-        loggedIn: account.jwt !== null,
-    };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    addNewBet: (bet: Bet) => dispatch(addNewBet(bet)),
-    placeBet: (num: number, value: number, gameType: number) => dispatch(placeBet(num, value, gameType)),
-    changeNum: (num: number) => dispatch(changeNum(num)),
-    changeValue: (value: number) => dispatch(changeValue(value)),
-    toggleHelp: (t: boolean) => dispatch(toggleHelp(t)),
-    showErrorMessage: (message: string) => dispatch(showErrorMessage(message)),
-    catchError: (error: Error) => catchError(error, dispatch),
-});
-
-export type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
-
-export interface OneDiceState {
-    showResult: boolean;
-    result: {num: number; won: boolean};
-}
-
-class ChooseFrom12 extends React.PureComponent<Props, OneDiceState> {
-    private loadedSounds = false;
-    private resultTimeoutId = 0;
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            showResult: false,
-            result: {num: 0, won: false},
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(resultTimeoutId.current);
         };
-    }
+    }, []);
 
-    public componentWillUnmount() {
-        clearTimeout(this.resultTimeoutId);
-    }
+    useEffect(() => {
+        return () => {
+            clearTimeout(resultTimeoutId.current);
+        };
+    }, []);
 
-    public componentDidUpdate(prevProps: Props) {
-        const {gameState, oneDice, changeValue} = this.props;
+    const {web3Available, gameState, info, oneDice, loggedIn} = useSelector(({games, account, web3}: State) => {
+        const {info, oneDice, gameState} = games;
+        const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
 
-        if (gameState.balance !== prevProps.gameState.balance) {
-            // if the balance changes, we need to check if user has enough funds for current bet value
-            const leftStake = gameState.stake + gameState.balance;
-            if (oneDice.value > leftStake) {
-                changeValue(Math.max(leftStake, MIN_BET_VALUE));
-            }
+        return {
+            web3Available: web3Available === true,
+            info,
+            oneDice,
+            gameState,
+            loggedIn: account.jwt !== null,
+        };
+    });
+
+    useEffect(() => {
+        // if the balance changes, we need to check if user has enough funds for current bet value
+        const leftStake = gameState.stake + gameState.balance;
+        if (oneDice.value > leftStake) {
+            changeValue(Math.max(leftStake, MIN_BET_VALUE));
         }
-    }
+    }, [gameState.stake, gameState.balance, oneDice.value]);
 
-    private onToggleHelp = () => {
-        const {toggleHelp, info} = this.props;
-        toggleHelp(!info.showHelp);
+    const dispatch = useDispatch();
+
+    const onToggleHelp = () => {
+        dispatch(toggleHelp(!info.showHelp));
     };
 
-    private onValueChange = (value: number) => {
-        const {changeValue} = this.props;
-        changeValue(value);
+    const onValueChange = (value: number) => {
+        dispatch(changeValue(value));
     };
 
-    private onClick = (diceNum: number) => {
-        const {info, oneDice, changeNum} = this.props;
-        const {showResult, result} = this.state;
+    const onClick = (diceNum: number) => {
         const {num} = oneDice;
 
         if (showResult && result.num === diceNum) {
-            this.setState({showResult: false});
+            setShowResult(false);
         }
 
         const newNum = (1 << diceNum) ^ num;
         if (info.sound) {
             playFromBegin(sounds.tileSelect);
         }
-        changeNum(newNum);
+        dispatch(changeNum(newNum));
     };
 
-    private onPlaceBet = () => {
-        const {info, oneDice, addNewBet, placeBet, catchError, showErrorMessage, web3Available, gameState, loggedIn} =
-            this.props;
-
+    const onPlaceBet = () => {
         const safeBetValue = Math.round(oneDice.value);
         const num = oneDice.num;
         const gameType = GameType.CHOOSE_FROM_12;
 
-        if (!this.loadedSounds) {
+        if (!loadedSounds.current) {
             // workaround for sound playback on mobile browsers: load sounds in user gesture handler
             sounds.win.load();
             sounds.lose.load();
-            this.loadedSounds = true;
+            loadedSounds.current = true;
         }
 
         const canBet = canPlaceBet(gameType, num, safeBetValue, loggedIn, web3Available, gameState);
         if (canBet.canPlaceBet) {
-            placeBet(num, safeBetValue, gameType)
+            dispatch(placeBet(num, safeBetValue, gameType))
                 .then((result) => {
-                    this.setState({result, showResult: true});
-                    clearTimeout(this.resultTimeoutId);
-                    this.resultTimeoutId = window.setTimeout(() => this.setState({showResult: false}), 5000);
+                    setResult(result);
+                    setShowResult(true);
+                    clearTimeout(resultTimeoutId.current);
+                    resultTimeoutId.current = window.setTimeout(() => setShowResult(false), 5000);
 
-                    addNewBet(result.bet);
+                    dispatch(addNewBet(result.bet));
                     if (info.sound) {
                         setTimeout(() => (result.won ? playFromBegin(sounds.win) : playFromBegin(sounds.lose)), 500);
                     }
                 })
-                .catch((error) => catchError(error));
+                .catch((error) => catchError(error, dispatch));
         } else {
-            showErrorMessage(canBet.errorMessage);
+            dispatch(showErrorMessage(canBet.errorMessage));
         }
     };
 
-    render() {
-        const {info, gameState, oneDice} = this.props;
-        const {num, value} = oneDice;
-        const {result, showResult} = this.state;
+    const {num, value} = oneDice;
 
-        let maxBetValue = Math.min(maxBet(GameType.CHOOSE_FROM_12, num, MIN_BANKROLL, KELLY_FACTOR), MAX_BET_VALUE);
-        if (gameState.status !== "ENDED") {
-            const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
-            maxBetValue = Math.max(max, MIN_BET_VALUE);
-        }
-
-        return (
-            <>
-                <Helmet>
-                    <title>Choose from 12 - Dicether</title>
-                    <meta name="description" content="Ethereum state channel based Choose from 12 game" />
-                </Helmet>
-                <Ui
-                    num={num}
-                    value={value}
-                    maxBetValue={maxBetValue}
-                    onValueChange={this.onValueChange}
-                    onClick={this.onClick}
-                    onPlaceBet={this.onPlaceBet}
-                    showResult={showResult}
-                    result={result}
-                    showHelp={info.showHelp}
-                    onToggleHelp={this.onToggleHelp}
-                />
-            </>
-        );
+    let maxBetValue = Math.min(maxBet(GameType.CHOOSE_FROM_12, num, MIN_BANKROLL, KELLY_FACTOR), MAX_BET_VALUE);
+    if (gameState.status !== "ENDED") {
+        const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
+        maxBetValue = Math.max(max, MIN_BET_VALUE);
     }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChooseFrom12);
+    return (
+        <>
+            <Helmet>
+                <title>Choose from 12 - Dicether</title>
+                <meta name="description" content="Ethereum state channel based Choose from 12 game" />
+            </Helmet>
+            <Ui
+                num={num}
+                value={value}
+                maxBetValue={maxBetValue}
+                onValueChange={onValueChange}
+                onClick={onClick}
+                onPlaceBet={onPlaceBet}
+                showResult={showResult}
+                result={result}
+                showHelp={info.showHelp}
+                onToggleHelp={onToggleHelp}
+            />
+        </>
+    );
+};
+
+export default ChooseFrom12;
