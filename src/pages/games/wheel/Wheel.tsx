c14ecff7476aa1bde3e9efb8af4bd88904e1067c
leroldary <leroldary@protonmail.com>
2025-12-27 17:43:41 +0100

    Convert Wheel to functional component

--- src/pages/games/wheel/Wheel.tsx
+++ src/pages/games/wheel/Wheel.tsx
@@ -1,105 +1,80 @@
 import {GameType, maxBet} from "@dicether/state-channel";
 import * as React from "react";
+import {useEffect, useRef, useState} from "react";
 import {Helmet} from "react-helmet";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 
 import {KELLY_FACTOR, MIN_BANKROLL, MIN_BET_VALUE} from "../../../config/config";
 import {addNewBet} from "../../../platform/modules/bets/asyncActions";
-import {Bet} from "../../../platform/modules/bets/types";
 import {toggleHelp} from "../../../platform/modules/games/info/actions";
 import {placeBet, validChainId} from "../../../platform/modules/games/state/asyncActions";
 import {showErrorMessage} from "../../../platform/modules/utilities/actions";
 import {catchError} from "../../../platform/modules/utilities/asyncActions";
 import {State} from "../../../rootReducer";
-import {Dispatch} from "../../../util/util";
 import sounds from "../sound";
 import {canPlaceBet} from "../utilities";
 import {changeNum, changeValue} from "./actions";
 import Ui from "./components/Ui";
 import {playFromBegin} from "../../../util/audio";
+import {useDispatch} from "../../../util/util";
 
-const mapStateToProps = ({games, account, web3, app}: State) => {
-    const {info, gameState, wheel} = games;
-    const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
+const Wheel = () => {
+    const loadedSounds = useRef(false);
+    const resultShowTimeoutId = useRef(0);
+    const resultUntilShowTimeoutId = useRef(0);
 
-    return {
-        web3Available: web3Available === true,
-        info,
-        gameState,
-        loggedIn: account.jwt !== null,
-        wheel,
-        nightMode: app.nightMode,
-    };
-};
+    const [spinning, setSpinning] = useState(false);
+    const [showResult, setShowResult] = useState(false);
+    const [result, setResult] = useState({betNum: 0, num: 0, won: false, userProfit: 0});
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    placeBet: (num: number, value: number, gameType: number) => dispatch(placeBet(num, value, gameType)),
-    addNewBet: (bet: Bet) => dispatch(addNewBet(bet)),
-    changeNum: (num: number) => dispatch(changeNum(num)),
-    changeValue: (value: number) => dispatch(changeValue(value)),
-    toggleHelp: (t: boolean) => dispatch(toggleHelp(t)),
-    showErrorMessage: (message: string) => dispatch(showErrorMessage(message)),
-    catchError: (error: Error) => catchError(error, dispatch),
-});
-
-export type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
-
-export interface WheelState {
-    spinning: boolean;
-    showResult: boolean;
-    result: {betNum: number; num: number; won: boolean; userProfit: number};
-}
-
-class Wheel extends React.PureComponent<Props, WheelState> {
-    private loadedSounds = false;
-    private resultShowTimeoutId = 0;
-    private resultUntilShowTimeoutId = 0;
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            spinning: false,
-            showResult: false,
-            result: {betNum: 0, num: 0, won: false, userProfit: 0},
+    useEffect(() => {
+        return () => {
+            clearTimeout(resultShowTimeoutId.current);
+            clearTimeout(resultUntilShowTimeoutId.current);
         };
-    }
+    }, []);
 
-    public componentWillUnmount() {
-        clearTimeout(this.resultShowTimeoutId);
-        clearTimeout(this.resultUntilShowTimeoutId);
-    }
+    const {web3Available, gameState, info, wheel, loggedIn, nightMode} = useSelector(
+        ({app, games, account, web3}: State) => {
+            const {gameState, info, wheel} = games;
+            const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
 
-    public componentDidUpdate(prevProps: Props) {
-        const {gameState, wheel, changeValue} = this.props;
+            return {
+                web3Available: web3Available === true,
+                gameState,
+                info,
+                wheel,
+                loggedIn: account.jwt !== null,
+                nightMode: app.nightMode,
+            };
+        },
+    );
 
-        if (gameState.balance !== prevProps.gameState.balance) {
-            // if the balance changes, we need to check if user has enough funds for current bet value
-            const leftStake = gameState.stake + gameState.balance;
-            if (wheel.value > leftStake) {
-                changeValue(Math.max(leftStake, MIN_BET_VALUE));
-            }
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // if the balance changes, we need to check if user has enough funds for current bet value
+        const leftStake = gameState.stake + gameState.balance;
+        if (wheel.value > leftStake) {
+            dispatch(changeValue(Math.max(leftStake, MIN_BET_VALUE)));
         }
-    }
+    }, [gameState.balance, gameState.stake, wheel.value]);
 
-    private onToggleHelp = () => {
-        const {toggleHelp, info} = this.props;
-        toggleHelp(!info.showHelp);
+    const onToggleHelp = () => {
+        dispatch(toggleHelp(!info.showHelp));
     };
 
-    private onPlaceBet = () => {
-        const {wheel, addNewBet, placeBet, catchError, showErrorMessage, web3Available, gameState, loggedIn} =
-            this.props;
-
+    const onPlaceBet = () => {
         const safeBetValue = Math.round(wheel.value);
         const num = wheel.num;
         const gameType = GameType.WHEEL;
 
-        if (!this.loadedSounds) {
+        if (!loadedSounds.current) {
             // workaround for sound playback on mobile browsers: load sounds in user gesture handler
             sounds.win.load();
             sounds.lose.load();
             sounds.menuDown.load();
-            this.loadedSounds = true;
+            loadedSounds.current = true;
         }
 
         if (num === 0) {
@@ -108,65 +83,56 @@ class Wheel extends React.PureComponent<Props, WheelState> {
 
         const canBet = canPlaceBet(gameType, num, safeBetValue, loggedIn, web3Available, gameState);
         if (canBet.canPlaceBet) {
-            this.setState({
-                showResult: false,
-            });
-            placeBet(num, safeBetValue, gameType)
+            setShowResult(false);
+            dispatch(placeBet(num, safeBetValue, gameType))
                 .then((result) => {
-                    this.setState({spinning: true, result});
-                    clearTimeout(this.resultUntilShowTimeoutId);
-                    this.resultUntilShowTimeoutId = window.setTimeout(() => {
-                        addNewBet(result.bet);
-                        this.setState({
-                            spinning: false,
-                            showResult: true,
-                            result,
-                        });
+                    setSpinning(true);
+                    setResult(result);
+                    clearTimeout(resultUntilShowTimeoutId.current);
+                    resultUntilShowTimeoutId.current = window.setTimeout(() => {
+                        dispatch(addNewBet(result.bet));
+                        setSpinning(false);
+                        setShowResult(true);
+                        setResult(result);
+                    }, 5000);
+
+                    clearTimeout(resultShowTimeoutId.current);
+                    resultShowTimeoutId.current = window.setTimeout(() => {
                         if (result.userProfit > 0) {
-                            this.playSound(sounds.win);
+                            playSound(sounds.win);
                         } else {
-                            this.playSound(sounds.lose);
+                            playSound(sounds.lose);
                         }
                     }, 5000);
 
-                    clearTimeout(this.resultShowTimeoutId);
-                    this.resultShowTimeoutId = window.setTimeout(
-                        () =>
-                            this.setState({
-                                showResult: false,
-                            }),
-                        10000,
-                    );
+                    clearTimeout(resultShowTimeoutId.current);
+                    resultShowTimeoutId.current = window.setTimeout(() => setShowResult(false), 10000);
                 })
-                .catch((error) => catchError(error));
+                .catch((error) => catchError(error, dispatch));
         } else {
-            showErrorMessage(canBet.errorMessage);
+            dispatch(showErrorMessage(canBet.errorMessage));
         }
     };
 
-    private onValueChange = (value: number) => {
-        const {changeValue} = this.props;
-        changeValue(value);
+    const onValueChange = (value: number) => {
+        dispatch(changeValue(value));
     };
 
-    private onRiskChange = (risk: number) => {
+    const onRiskChange = (risk: number) => {
         // TODO: as action
-        const {wheel, changeNum} = this.props;
         const {num} = wheel;
         const newNum = risk * 100 + (num % 100);
-        changeNum(newNum);
+        dispatch(changeNum(newNum));
     };
 
-    private onSegmentsChange = (segments: number) => {
+    const onSegmentsChange = (segments: number) => {
         // TODO: as action
-        const {wheel, changeNum} = this.props;
         const {num} = wheel;
         const newNum = Math.floor(num / 100) * 100 + segments;
-        changeNum(newNum);
+        dispatch(changeNum(newNum));
     };
 
-    private playSound = (audio: HTMLAudioElement) => {
-        const {info} = this.props;
+    const playSound = (audio: HTMLAudioElement) => {
         const {sound} = info;
 
         if (sound) {
@@ -174,42 +140,37 @@ class Wheel extends React.PureComponent<Props, WheelState> {
         }
     };
 
-    render() {
-        const {nightMode, info, gameState, wheel} = this.props;
-        const {num, value} = wheel;
-        const {result, showResult, spinning} = this.state;
-
-        let maxBetValue = maxBet(GameType.WHEEL, num === 0 ? 1 : num, MIN_BANKROLL, KELLY_FACTOR);
-        if (gameState.status !== "ENDED") {
-            const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
-            maxBetValue = Math.max(max, MIN_BET_VALUE);
-        }
-
-        return (
-            <>
-                <Helmet>
-                    <title>Wheel - Dicether</title>
-                    <meta name="description" content="Ethereum state channel based Wheel game" />
-                </Helmet>
-                <Ui
-                    nightMode={nightMode}
-                    risk={Math.floor(num / 100)}
-                    segments={num % 100}
-                    value={value}
-                    maxBetValue={maxBetValue}
-                    onValueChange={this.onValueChange}
-                    onPlaceBet={this.onPlaceBet}
-                    onRiskChange={this.onRiskChange}
-                    onSegmentsChange={this.onSegmentsChange}
-                    showResult={showResult}
-                    disableRiskSegmentUpdate={spinning}
-                    result={{...result}}
-                    showHelp={info.showHelp}
-                    onToggleHelp={this.onToggleHelp}
-                />
-            </>
-        );
+    const {num, value} = wheel;
+    let maxBetValue = maxBet(GameType.WHEEL, num === 0 ? 1 : num, MIN_BANKROLL, KELLY_FACTOR);
+    if (gameState.status !== "ENDED") {
+        const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
+        maxBetValue = Math.max(max, MIN_BET_VALUE);
     }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Wheel);
+    return (
+        <>
+            <Helmet>
+                <title>Wheel - Dicether</title>
+                <meta name="description" content="Ethereum state channel based Wheel game" />
+            </Helmet>
+            <Ui
+                nightMode={nightMode}
+                risk={Math.floor(num / 100)}
+                segments={num % 100}
+                value={value}
+                maxBetValue={maxBetValue}
+                onValueChange={onValueChange}
+                onPlaceBet={onPlaceBet}
+                onRiskChange={onRiskChange}
+                onSegmentsChange={onSegmentsChange}
+                showResult={showResult}
+                disableRiskSegmentUpdate={spinning}
+                result={{...result}}
+                showHelp={info.showHelp}
+                onToggleHelp={onToggleHelp}
+            />
+        </>
+    );
+};
+
+export default Wheel;
