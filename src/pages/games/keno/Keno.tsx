3390a8723645fa6ecaa506701adeb1b5d7713039
leroldary <leroldary@protonmail.com>
2025-12-27 15:40:24 +0100

    Convert Keno to functional component

--- src/pages/games/keno/Keno.tsx
+++ src/pages/games/keno/Keno.tsx
@@ -8,56 +8,24 @@ import {
 } from "@dicether/state-channel";
 import BN from "bn.js";
 import * as React from "react";
+import {useEffect, useRef, useState} from "react";
 import {Helmet} from "react-helmet";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 
 import {KELLY_FACTOR, MIN_BANKROLL, MIN_BET_VALUE} from "../../../config/config";
 import {addNewBet} from "../../../platform/modules/bets/asyncActions";
-import {Bet} from "../../../platform/modules/bets/types";
 import {toggleHelp} from "../../../platform/modules/games/info/actions";
 import {placeBet, validChainId} from "../../../platform/modules/games/state/asyncActions";
 import {showErrorMessage} from "../../../platform/modules/utilities/actions";
 import {catchError} from "../../../platform/modules/utilities/asyncActions";
 import {State} from "../../../rootReducer";
 import {getRandomInt, shuffle} from "../../../util/math";
-import {Dispatch} from "../../../util/util";
+import {useDispatch} from "../../../util/util";
 import sounds from "../sound";
 import {canPlaceBet} from "../utilities";
 import {changeNum, changeValue} from "./actions";
 import Ui from "./components/Ui";
 import {playFromBegin} from "../../../util/audio";
-
-const mapStateToProps = ({games, account, web3}: State) => {
-    const {info, keno, gameState} = games;
-    const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
-
-    return {
-        web3Available: web3Available === true,
-        info,
-        keno,
-        gameState,
-        loggedIn: account.jwt !== null,
-    };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    placeBet: (num: number, value: number, gameType: number) => dispatch(placeBet(num, value, gameType)),
-    addNewBet: (bet: Bet) => dispatch(addNewBet(bet)),
-    changeNum: (num: number) => dispatch(changeNum(num)),
-    changeValue: (value: number) => dispatch(changeValue(value)),
-    toggleHelp: (t: boolean) => dispatch(toggleHelp(t)),
-    showErrorMessage: (message: string) => dispatch(showErrorMessage(message)),
-    catchError: (error: Error) => catchError(error, dispatch),
-});
-
-export type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
-
-export interface KenoState {
-    showResult: boolean;
-    tmpResult: {betNum: number; num: number; won: boolean; userProfit: number};
-
-    showResultProfit: boolean;
-}
 
 interface Result {
     betNum: number;
@@ -66,51 +34,54 @@ interface Result {
     userProfit: number;
 }
 
-class Keno extends React.PureComponent<Props, KenoState> {
-    private loadedSounds = false;
-    private pickTimeoutId = 0;
-    private resultTimeoutId = 0;
+const Keno = () => {
+    const loadedSounds = useRef(false);
+    const pickTimeoutId = useRef(0);
+    const resultTimeoutId = useRef(0);
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            showResult: false,
-            //result: {betNum: 0, num: 0, won: false, userProfit: 0},
-            tmpResult: {betNum: 0, num: 0, won: false, userProfit: 0},
-            showResultProfit: false,
+    const [showResult, setShowResult] = useState(false);
+    const [tmpResult, setTmpResult] = useState({betNum: 0, num: 0, won: false, userProfit: 0});
+    const [showResultProfit, setShowResultProfit] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(pickTimeoutId.current);
+            clearTimeout(resultTimeoutId.current);
         };
-    }
+    }, []);
 
-    public componentWillUnmount() {
-        clearTimeout(this.pickTimeoutId);
-        clearTimeout(this.resultTimeoutId);
-    }
+    const {web3Available, gameState, info, keno, loggedIn} = useSelector(({games, account, web3}: State) => {
+        const {gameState, info, keno} = games;
+        const web3Available = web3.account && web3.contract && web3.web3 && validChainId(web3.chainId);
 
-    public componentDidUpdate(prevProps: Props) {
-        const {gameState, keno, changeValue} = this.props;
+        return {
+            web3Available: web3Available === true,
+            gameState,
+            info,
+            keno,
+            loggedIn: account.jwt !== null,
+        };
+    });
 
-        if (gameState.balance !== prevProps.gameState.balance) {
-            // if the balance changes, we need to check if user has enough funds for current bet value
-            const leftStake = gameState.stake + gameState.balance;
-            if (keno.value > leftStake) {
-                changeValue(Math.max(leftStake, MIN_BET_VALUE));
-            }
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // if the balance changes, we need to check if user has enough funds for current bet value
+        const leftStake = gameState.stake + gameState.balance;
+        if (keno.value > leftStake) {
+            changeValue(Math.max(leftStake, MIN_BET_VALUE));
         }
-    }
+    }, [gameState.stake, gameState.balance, keno.value]);
 
-    private onToggleHelp = () => {
-        const {toggleHelp, info} = this.props;
+    const onToggleHelp = () => {
         toggleHelp(!info.showHelp);
     };
 
-    private onValueChange = (value: number) => {
-        const {changeValue} = this.props;
+    const onValueChange = (value: number) => {
         changeValue(value);
     };
 
-    private onClick = (tile: number) => {
-        const {keno, changeNum} = this.props;
-        const {showResult} = this.state;
+    const onClick = (tile: number) => {
         const {num} = keno;
 
         if (showResult) {
@@ -118,14 +89,11 @@ class Keno extends React.PureComponent<Props, KenoState> {
         }
 
         const newNum = new BN(num).xor(new BN(1).shln(tile)).toNumber();
-        this.playSound(sounds.tileSelect);
-        changeNum(newNum);
+        playSound(sounds.tileSelect);
+        dispatch(changeNum(newNum));
     };
 
-    private onAutoPick = () => {
-        const {keno, changeNum} = this.props;
-        const {num} = keno;
-
+    const onAutoPickHelper = (num: number) => {
         if (getNumSetBits(num) === KENO_SELECTABLE_FIELDS) {
             return;
         }
@@ -138,31 +106,31 @@ class Keno extends React.PureComponent<Props, KenoState> {
         } while (numBN.and(oneBN.shln(tile)).toNumber() !== 0);
 
         const newNum = numBN.xor(oneBN.shln(tile)).toNumber();
-        changeNum(newNum);
-        this.playSound(sounds.tileSelect);
+        dispatch(changeNum(newNum));
+        playSound(sounds.tileSelect);
 
-        this.pickTimeoutId = window.setTimeout(this.onAutoPick, 100);
+        pickTimeoutId.current = window.setTimeout(() => onAutoPickHelper(newNum), 100);
     };
 
-    private onClear = () => {
-        const {changeNum} = this.props;
-        changeNum(0);
+    const onAutoPick = () => {
+        onAutoPickHelper(0);
     };
 
-    private onPlaceBet = () => {
-        const {keno, addNewBet, placeBet, catchError, showErrorMessage, web3Available, gameState, loggedIn} =
-            this.props;
+    const onClear = () => {
+        dispatch(changeNum(0));
+    };
 
+    const onPlaceBet = () => {
         const safeBetValue = Math.round(keno.value);
         const num = keno.num;
         const gameType = GameType.KENO;
 
-        if (!this.loadedSounds) {
+        if (!loadedSounds.current) {
             // workaround for sound playback on mobile browsers: load sounds in user gesture handler
             sounds.tileHit.load();
             sounds.tileMiss.load();
             sounds.tileSelect.load();
-            this.loadedSounds = true;
+            loadedSounds.current = true;
         }
 
         if (num === 0) {
@@ -171,30 +139,26 @@ class Keno extends React.PureComponent<Props, KenoState> {
 
         const canBet = canPlaceBet(gameType, num, safeBetValue, loggedIn, web3Available, gameState);
         if (canBet.canPlaceBet) {
-            placeBet(num, safeBetValue, gameType)
+            dispatch(placeBet(num, safeBetValue, gameType))
                 .then((result) => {
-                    this.setState({showResult: true, tmpResult: {...result, num: 0}});
-                    clearTimeout(this.resultTimeoutId);
-                    this.resultTimeoutId = window.setTimeout(
-                        () =>
-                            this.setState({
-                                showResult: false,
-                                showResultProfit: false,
-                            }),
-                        5000,
-                    );
+                    setShowResult(true);
+                    setTmpResult({...result, num: 0});
+                    clearTimeout(resultTimeoutId.current);
+                    resultTimeoutId.current = window.setTimeout(() => {
+                        setShowResult(false);
+                        setShowResultProfit(false);
+                    }, 5000);
 
-                    addNewBet(result.bet);
-                    this.showResult(result);
+                    dispatch(addNewBet(result.bet));
+                    showKenoResult(result);
                 })
-                .catch((error) => catchError(error));
+                .catch((error) => catchError(error, dispatch));
         } else {
             showErrorMessage(canBet.errorMessage);
         }
     };
 
-    private showResultHelper = (indices: number[], betNum: number, curNum: number) => {
-        //const {tmpResult, result} = this.state;
+    const showResultHelper = (indices: number[], betNum: number, curNum: number) => {
         if (indices.length === 0) {
             return;
         }
@@ -203,39 +167,36 @@ class Keno extends React.PureComponent<Props, KenoState> {
         const newBit = new BN(1).shln(idx);
         const newTmpResult = new BN(curNum).or(newBit).toNumber();
 
-        this.setState((prevState) => ({
-            tmpResult: {
-                ...prevState.tmpResult,
-                num: newTmpResult,
-            },
+        setTmpResult((prevTmpResult) => ({
+            ...prevTmpResult,
+            num: newTmpResult,
         }));
 
         if (newBit.and(new BN(betNum)).toNumber() !== 0) {
-            this.playSound(sounds.tileHit);
+            playSound(sounds.tileHit);
         } else {
-            this.playSound(sounds.tileMiss);
+            playSound(sounds.tileMiss);
         }
 
         indices.shift();
 
         if (indices.length === 0) {
-            window.setTimeout(() => this.setState({showResultProfit: true}), 300);
+            window.setTimeout(() => setShowResultProfit(true), 300);
         } else {
-            window.setTimeout(this.showResultHelper, 300, indices, betNum, newTmpResult);
+            window.setTimeout(showResultHelper, 300, indices, betNum, newTmpResult);
         }
     };
 
-    private showResult = (result: Result) => {
+    const showKenoResult = (result: Result) => {
         const {num} = result;
         const resultTiles = getSetBits(num);
 
         const indices = resultTiles.map((x, i) => (x ? i : -1)).filter((idx) => idx !== -1);
         shuffle(indices);
-        this.showResultHelper(indices, result.betNum, 0);
+        showResultHelper(indices, result.betNum, 0);
     };
 
-    private playSound = (audio: HTMLAudioElement) => {
-        const {info} = this.props;
+    const playSound = (audio: HTMLAudioElement) => {
         const {sound} = info;
 
         if (sound) {
@@ -243,41 +204,37 @@ class Keno extends React.PureComponent<Props, KenoState> {
         }
     };
 
-    render() {
-        const {info, gameState, keno} = this.props;
-        const {num, value} = keno;
-        const {showResult, tmpResult, showResultProfit} = this.state;
+    const {num, value} = keno;
 
-        let maxBetValue = maxBet(GameType.KENO, num === 0 ? 1 : num, MIN_BANKROLL, KELLY_FACTOR);
-        if (gameState.status !== "ENDED") {
-            const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
-            maxBetValue = Math.max(max, MIN_BET_VALUE);
-        }
-
-        return (
-            <>
-                <Helmet>
-                    <title>Keno - Dicether</title>
-                    <meta name="description" content="Ethereum state channel based keno game" />
-                </Helmet>
-                <Ui
-                    num={num}
-                    value={value}
-                    maxBetValue={maxBetValue}
-                    onValueChange={this.onValueChange}
-                    onClick={this.onClick}
-                    onAutoPick={this.onAutoPick}
-                    onClear={this.onClear}
-                    onPlaceBet={this.onPlaceBet}
-                    showResult={showResult}
-                    showResultProfit={showResultProfit}
-                    result={tmpResult}
-                    showHelp={info.showHelp}
-                    onToggleHelp={this.onToggleHelp}
-                />
-            </>
-        );
+    let maxBetValue = maxBet(GameType.KENO, num === 0 ? 1 : num, MIN_BANKROLL, KELLY_FACTOR);
+    if (gameState.status !== "ENDED") {
+        const max = Math.min(gameState.stake + gameState.balance, maxBetValue);
+        maxBetValue = Math.max(max, MIN_BET_VALUE);
     }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Keno);
+    return (
+        <>
+            <Helmet>
+                <title>Keno - Dicether</title>
+                <meta name="description" content="Ethereum state channel based keno game" />
+            </Helmet>
+            <Ui
+                num={num}
+                value={value}
+                maxBetValue={maxBetValue}
+                onValueChange={onValueChange}
+                onClick={onClick}
+                onAutoPick={onAutoPick}
+                onClear={onClear}
+                onPlaceBet={onPlaceBet}
+                showResult={showResult}
+                showResultProfit={showResultProfit}
+                result={tmpResult}
+                showHelp={info.showHelp}
+                onToggleHelp={onToggleHelp}
+            />
+        </>
+    );
+};
+
+export default Keno;
